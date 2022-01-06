99ab57c3894a3049ec0c125793d18cceece7c7b1
Yash Chauhan <yc9274@gmail.com>
2022-01-06 07:16:42 +0530

    Removed: everything

--- android/app/src/main/kotlin/com/example/rainbow/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.bsteps.rainbow
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utility/helpers/color.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:math';
-
-String randomHexColor() {
-  var _hex = '';
-  do {
-    _hex = (Random().nextDouble() * 16777215).round().toRadixString(16);
-  } while (_hex.length < 6);
-  return _hex;
-}
